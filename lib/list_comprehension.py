455fa4840378a8c2b746a0600e257e0a970be690
Ngeny Gilbert <ngenygkip@gmail.com>
2024-09-01 11:08:42 +0300

    comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even = []
+    for num in num_list:
+        if num % 2 == 0:
+            even.append(num)
+    return even
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = []
+    for item in sentence_list:
+        new_list.append(item + '!') 
+    return new_list
